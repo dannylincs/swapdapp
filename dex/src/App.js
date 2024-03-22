93bbf2a47f1eeedba3db96831b3548eea1501055
dannylincs <dannysocrate@gmail.com>
2024-03-22 12:33:19 +0100

    first

---
+++ dex/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Header from "./components/Header"; 
+import Swap from "./components/Swap";
+import Tokens from "./components/Tokens";
+import { Routes, Route } from "react-router-dom";
+import { useConnect, useAccount } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+function App() {
+  const { address, isConnectd } = useAccount();
+  const { connect } = useConnect({
+    connector: new MetaMaskConnector(),
+  });
+  
+  return(
+    <div className="App">
+      <Header connect = {connect} isConnected = {isConnectd} address= {address} />
+      <div className="mainWindow">
+        <Routes>
+          <Route path="/" element={<Swap  isConnected = {isConnectd} address= {address} />} />
+          <Route path="/tokens" element={<Tokens />} />
+        </Routes>
+      </div>
+    </div>
+  ) 
+}
+
+export default App;
